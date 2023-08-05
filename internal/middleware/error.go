cdb3a8c06ac11c5ce3176d8dbe74f903cebc204e
Daniel Mesquita <danielmesquitta123@gmail.com>
2023-08-05 09:41:46 -0300

    feat: error handling

---
+++ internal/middleware/error.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"errors"
+
+	"github.com/danielmesquitta/password-manager-api/internal/controller/response"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+)
+
+func ErrorHandlerMiddleware(ctx *fiber.Ctx, err error) error {
+	code := fiber.StatusInternalServerError
+
+	var e *fiber.Error
+	if errors.As(err, &e) {
+		code = e.Code
+	}
+
+	log.Error(err)
+
+	return ctx.Status(code).JSON(response.ErrorResponse{
+		Message:    err.Error(),
+		StatusCode: code,
+	})
+}
